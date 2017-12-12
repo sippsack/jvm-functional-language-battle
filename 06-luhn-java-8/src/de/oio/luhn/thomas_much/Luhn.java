dcdd1a1f4a80f5b1c9a29029b9e186c109e1a3dc
Falk Sippach <falk@jug-da.de>
2017-12-12 14:44:51 +0100

    new example by Thomas Much

--- 06-luhn-java-8/src/de/oio/luhn/thomas_much/Luhn.java
+++ 06-luhn-java-8/src/de/oio/luhn/thomas_much/Luhn.java
@@ -3,24 +3,39 @@ package de.oio.luhn.thomas_much;
 import java.util.PrimitiveIterator;
 import java.util.function.Function;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Luhn {
-	private static Function<String, String> reverse = number -> new StringBuilder(number).reverse().toString(); 
-	
+	private static Function<String, String> reverse = number -> new StringBuilder(number).reverse().toString();
+
 	public static boolean isValid(String number) {
 		PrimitiveIterator.OfInt faktor = IntStream.iterate(1, i -> 3 - i).iterator();
-		return (new StringBuilder(number)
+		return new StringBuilder(number)
 				.reverse()
 				.chars()
 				.map(c -> faktor.nextInt() * (c - '0'))
-				.reduce(0, (a, b) -> a + b / 10 + b % 10) % 10) == 0;
+				.reduce(0, (a, b) -> a + b / 10 + b % 10) 
+				% 10 == 0;
 	}
-	
+
+	public static boolean isValid(long number) {
+		PrimitiveIterator.OfInt faktor = IntStream.iterate(1, i -> 3 - i).iterator();
+		return LongStream.iterate(number, n -> n / 10)
+				.takeWhile(n -> n > 0)
+				.map(n -> n % 10)
+				.map(n -> n * faktor.nextInt())
+				.map(n -> n > 9 ? n - 9 : n).sum()
+				% 10 == 0;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(reverse.apply("1234567"));
 		System.out.println(isValid("23456"));
+		System.out.println(isValid(23456L));
 		System.out.println(isValid("234567"));
+		System.out.println(isValid(234567L));
 		System.out.println(isValid("4012888888881881"));
+		System.out.println(isValid(4012888888881881L));
 		PrimitiveIterator.OfInt faktor = IntStream.iterate(1, i -> 3 - i).iterator();
 		System.out.println(faktor.nextInt());
 		System.out.println(faktor.nextInt());
